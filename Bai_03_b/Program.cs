ed417e4241cf88f014f6ae162548aae033445a22
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-02-28 21:47:49 +0700

    update

---
+++ Bai_03_b/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_03_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b;
+            Console.WriteLine("Mời nhập a: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Mời nhập b: ");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine($"a + b = {a+b}");
+            Console.WriteLine($"a - b = {a-b}");
+            Console.WriteLine($"a x b = {a*b}");
+            Console.WriteLine($"a / b = {a/b}");
+            Console.ReadLine();
+        }
+    }
+}
