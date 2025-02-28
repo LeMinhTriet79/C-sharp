31f1f0ce001bab480bcffd15850b85e386cee17e
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-02-28 16:04:19 +0700

    Bai_02_Basic_Variables
    Các kiểu cơ bản giống Java

--- Bai_01_Khai_De/Program.cs
+++ Bai_01_Khai_De/Program.cs
@@ -14,6 +14,8 @@ namespace Bai_01_Khai_De
             Console.OutputEncoding = Encoding.UTF8; //Cho phép viết tiếng Việt có dấu!
             Console.WriteLine("Đây là phần mở đầu - Khai Đề C#");
             Console.WriteLine("Xin chào bà con cô bác!\nTui là Lê Minh Triết.");
+            Console.ReadLine();
+           
         }
     }
 }
---
+++ Bai_02_Basic_Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_02_Basic_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string name = "Nguyễn Tuấn Thanh";
+            int age = 21;
+            double gpa = 9.5f;
+
+            //Kiểu đặc trưng của C#.
+            Console.WriteLine("Cách thứ nhứt:");
+            Console.WriteLine("Họ và tên: {0}\nTuổi: {1}\nĐiểm trung bình: {2}", name, age, gpa);
+            //Giống Java.
+            Console.WriteLine("\nCách thứ nhì:");
+            Console.WriteLine("Họ và tên: " + name + "\nTuổi: " + age +
+                "\nĐiểm trung bình: " +  gpa);
+
+            Console.ReadKey(); // để cho đẹp
+        }
+    }
+}
