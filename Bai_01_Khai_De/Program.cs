f2d9d6e1e4ae2fbf46b1292835894378e63c9221
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-02-28 15:21:45 +0700

    Bai_01_Khai_De
    Phần Khai Đề - nhập môn C#

---
+++ Bai_01_Khai_De/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bai_01_Khai_De
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; //Cho phép viết tiếng Việt có dấu!
+            Console.WriteLine("Đây là phần mở đầu - Khai Đề C#");
+            Console.WriteLine("Xin chào bà con cô bác!\nTui là Lê Minh Triết.");
+        }
+    }
+}
