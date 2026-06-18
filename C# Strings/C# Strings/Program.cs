907c5e612e0a51c642695323a8b93fdbd6e76a92
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 13:06:35 +0700

    C# Strings

---
+++ C# Strings/C# Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+
+            string greeting2 = "Nice to meet you!";
+
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+            string txt1 = "Hello World";
+            Console.WriteLine(txt1.ToUpper());   // Outputs "HELLO WORLD"
+            Console.WriteLine(txt1.ToLower());   // Outputs "hello world"
+        }
+    }
+}
