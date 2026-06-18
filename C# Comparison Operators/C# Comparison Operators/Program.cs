deb3738401d69ad1ecb019c40454466e3f3f285b
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 13:00:06 +0700

    up

---
+++ C# Comparison Operators/C# Comparison Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Comparison_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x > y); // returns True because 5 is greater than 3
+        }
+    }
+}
