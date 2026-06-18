686b7d5e0b0e1030e2dd035e85ceeb85f8c271bc
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 12:59:00 +0700

    up

---
+++ C# Operators/C# Operators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 100 + 50;        // 150 (100 + 50)
+            int sum2 = sum1 + 250;      // 400 (150 + 250)
+            int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+        }
+    }
+}
