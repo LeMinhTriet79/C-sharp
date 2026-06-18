9d2af86501c391b0d28aaf1be06d127d24235d93
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 13:04:46 +0700

    up

---
+++ C# Math/C# Math/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Min(5, 10));
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(Math.Abs(-4.7));
+            Console.WriteLine(Math.Round(9.99)); 
+
+
+        }
+    }
+}
