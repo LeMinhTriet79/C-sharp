9a6a9edccbf5b3446117adff0e550b9167d56faf
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 11:31:00 +0700

    Hoc them

---
+++ Play-With-Constants/Constants/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int a = 9;
+            const int b = 10;
+            Console.WriteLine(a + b);
+        }
+    }
+}
