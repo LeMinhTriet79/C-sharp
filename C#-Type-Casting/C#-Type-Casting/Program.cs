48012b17ab8d8aba166015ed121ce9ea0d6a8fc7
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 12:53:20 +0700

    up

---
+++ C#-Type-Casting/C#-Type-Casting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Type_Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 9;
+            double myDouble = myInt;
+            Console.WriteLine(myInt);
+            Console.WriteLine(myDouble);
+
+            double a = 9.79;
+            int b = (int)a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int c = 10;
+            double d = 5.25;
+            bool e = true;
+
+            Console.WriteLine(Convert.ToString(c));    // convert int to string
+            Console.WriteLine(Convert.ToDouble(c));    // convert int to double
+            Console.WriteLine(Convert.ToInt32(d));  // convert double to int
+            Console.WriteLine(Convert.ToString(e));   // convert bool to string
+        }
+    }
+    
+}
