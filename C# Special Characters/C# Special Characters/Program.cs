5ee72d1315d36f57277d30c16147f24bf5cab06c
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 13:20:25 +0700

    up

---
+++ C# Special Characters/C# Special Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Special_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string txt = "We are the so-called "Vikings" from the north."; //error
+
+            //The solution to avoid this problem, is to use the backslash escape character.
+
+            string txt = "We are the so-called \"Vikings\" from the north.";
+
+            string txt1 = "The character \\ is called backslash."; // -> \ ...
+
+
+
+
+        }
+    }
+}
