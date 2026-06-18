8bf6960b25481a2218345808ccded9f5a466de65
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-06-18 12:43:41 +0700

    Constants variables
    Constants

--- Play-With-Constants/Play-With-Constants/Program.cs
+++ Play-With-Constants/Play-With-Constants/Program.cs
@@ -12,7 +12,14 @@ namespace Play_With_Constants
         {
             const int a = 9;
             const int b = 10;
+            int c = 11;
             Console.WriteLine(a + b);
+            c = 39;
+            Console.WriteLine($"c = {c}");
+            //Hằng số không cho phép thay đổi là lỗi ngay, vì có chữ const
+            //a = 0;
+            //Console.WriteLine(a);
+
         }
     }
 }
